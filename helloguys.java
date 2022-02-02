36273be01d8cc590e245bfeb3a94c56bd497d31f
yogeshwaran <yogeshwaran.ma@gmail.com>
2022-02-02 11:56:01 +0100

    first commit

---
+++ helloguys.java
@@ -0,0 +1,13 @@
+public class helloguys {
+           
+      
+       public static void main(String[] args) {
+           
+              System.out.println("Hey ajay");
+              System.out.println("Hey arun");
+              System.out.println("Hey akash");
+              System.out.println("Hey avinash");
+          
+       }
+
+}
